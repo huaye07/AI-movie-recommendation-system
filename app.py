430c87e83ef9d03ef75f3ea16c9e9940d9cfabfe
Huaye Li <166847183+huaye07@users.noreply.github.com>
2025-04-07 13:53:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -100,6 +100,7 @@ html, body, .stApp {
     height: 100%;
     margin: 0;
     background-color: #f39c12;  /* A warm yellow-orange gradient background */
+    background-image: url('https://1000logos.net/wp-content/uploads/2022/11/Clemson-Tigers-Logo.png');
     font-family: 'Outfit', sans-serif;
 }
 
